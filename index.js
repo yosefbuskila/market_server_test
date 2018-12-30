b77ec740ebf5d0e86f239d3f9c753ddbf7306f53
yosefbuskila <yosef3053@gmail.com>
2018-12-30 16:20:34 +0200

    30/1/18

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
+
+const usersConמect=require('./lib/users/userManagement')
+
 const port = 3000;
 let verified=false;
 
 app.use(function (req, res, next) {
     console.log('Time:', Date.now())
-    if (verified)
-    next()
-    else
-    res.send('not verified')
+    usersConמect.reg(['yoszeezf@gma','1234','yos','bus','null','null','mirski','5','jer','isr','3053'])
+    .then(resp=>next())
+    .catch(function (resp){console.log(resp);res.send('not verified');} )
   })
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
