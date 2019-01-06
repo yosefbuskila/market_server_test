b0e1611da4d7bb74d72c801280d7947ed16fe1b9
yosefbuskila <yosef3053@gmail.com>
2019-01-06 17:17:49 +0200

    6/1/19

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/user', userRouter);
-
+ 
 const port = 3000;
 
 app.use(function (req, res, next) {
     console.log('Time:', Date.now(),req.body)
-    usersConnect.chakConnect(req.body.id, req.body.token).then((ans) => {console.log('OK');if (ans==='OK') next();},
+    usersConnect.chakConnect(req.body.id, req.body.token).then((ans) => {
+      // console.log('[ ',ans ,' ]');
+      req.client=ans;
+      if (ans[0].id) next();
+    },
     function ()  {console.log('notOK');res.send('not verified');})    
   })
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => res.send('Hello World post!'))
+app.post('/', (req, res) =>{ res.send('Hello World post!')
+
+}   );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
